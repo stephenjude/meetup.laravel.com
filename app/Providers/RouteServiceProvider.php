8f6dada4f56e2086799bd15a4c7714b376ad5430
Stephen Jude <StephenJudesuccess@gmail.com>
2020-08-14 06:17:58 +0100

    clean up

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -9,24 +9,24 @@ class RouteServiceProvider extends ServiceProvider
 {
     public const HOME = '/home';
 
-    public function map()
-    {
-        $this->mapApiRoutes();
+    // public function map()
+    // {
+    //     $this->mapApiRoutes();
 
-        $this->mapWebRoutes();
+    //     $this->mapWebRoutes();
 
-        //
-    }
+    //     //
+    // }
 
-    protected function mapWebRoutes()
-    {
-        Route::middleware('web')->group(base_path('routes/web.php'));
-    }
+    // protected function mapWebRoutes()
+    // {
+    //     Route::middleware('web')->group(base_path('routes/web.php'));
+    // }
 
-    protected function mapApiRoutes()
-    {
-        Route::prefix('api')
-            ->middleware('api')
-            ->group(base_path('routes/api.php'));
-    }
+    // protected function mapApiRoutes()
+    // {
+    //     Route::prefix('api')
+    //         ->middleware('api')
+    //         ->group(base_path('routes/api.php'));
+    // }
 }
